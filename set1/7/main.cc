5e91e500a63f41375e63b9c6af349e846d9c0462
swenjohannes <s.johannes@student.rug.nl>
2021-02-05 20:11:06 +0100

    Seems to work

---
+++ set1/7/main.cc
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <set>
+    
+using namespace std;
+    
+
+//template<typename ...Params>
+//ostream & operator << (ostream &out, ...Params) 
+
+template<typename T>
+ostream & operator << (ostream &out, set<T> const Set) 
+{ 
+    out << '{';                          //the first opening bracket
+    for (auto elem : Set)
+    {
+        out << elem;                    // display elems
+        if (elem != *Set.rbegin())      
+            out << ", ";                // sep except for last elem
+    }
+    out << '}';                         //closing bracket of the set
+    return out;
+}
+
+int main()
+{
+    set<size_t> const universe = { 1, 2, 3, 4};
+    set<set<size_t>> const subsets =
+    {
+        { 1, 2,      },
+        {       3, 4 },
+    };
+
+        cout << "Universe: " << universe << '\n'
+                << "subsets: " << subsets << '\n';  // WC: no operator<<
+                            // desired output: Universe: {1, 2, 3, 4} -> it does that
+                            //                 subsets: {{1, 2}, {3, 4}} -> it does that
+}
