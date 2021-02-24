1c492a7654ce51ca70342dc7bcc3166ec90c598e
Timen van Gelderen <tvgelderen@outlook.com>
2021-02-24 19:01:45 +0100

    updated 21

--- set3/21/chars/chars.h
+++ set3/21/chars/chars.h
@@ -1,52 +1,38 @@
 #ifndef INCLUDED_CHARS_
 #define INCLUDED_CHARS_
 
-#include <vector>
 #include <ostream>
 
 template <char ch = ' ', char ...Params>  // default first char is a space
 struct Chars
 {
-    std::vector<char> d_chars;
-
-    Chars();
-
-    template <char ...Args>
-    friend std::ostream &operator<<(std::ostream &out, Chars<Args ...> const &chars);
+    template <char arg, char ...Args>
+    friend std::ostream &operator<<(std::ostream &out,
+                                    Chars<arg, Args ...> const &chars);
 };
 
 template <char ch, char ...Params>
-inline Chars<ch, Params ...>::Chars()
+inline std::ostream &operator<<(std::ostream &out,
+                                    Chars<ch, Params ...> const &chars)
 {
-    d_chars.push_back(ch);      // add the char to the vector
-    Chars<Params ...> chars;    // create a Chars object with the other chars
-                        // append the other chars to the current vector
-    d_chars.insert(d_chars.end(), chars.d_chars.begin(), chars.d_chars.end());
-}
-
-template <char ...Params>
-inline std::ostream &operator<<(std::ostream &out, Chars<Params ...> const &chars)
-{
-    for (size_t idx = 0; idx != chars.d_chars.size(); ++idx)
-        out << chars.d_chars[idx];
-
+    out << ch;
+    out << Chars<Params ...>();
     return out;
 }
 
-template <char ch>              // specialized for the last char
+template <char ch>                        // specialized for the last char
 struct Chars<ch>
 {
-    std::vector<char> d_chars;
-
-    Chars();
+    template <char arg>
+    friend std::ostream &operator<<(std::ostream &out,
+                                    Chars<arg> const &chars);
 };
 
 template <char ch>
-inline Chars<ch>::Chars()
+inline std::ostream &operator<<(std::ostream &out, Chars<ch> const &chars)
 {
-    d_chars.push_back(ch);
+    out << ch;
+    return out;
 }
-
-
 
 #endif
