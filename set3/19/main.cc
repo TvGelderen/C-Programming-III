b987cdc5eb63b11dc0b2ac0167a4cd1f2a9e7a67
swenjohannes <s.johannes@student.rug.nl>
2021-03-01 21:50:07 +0100

    initial solution

---
+++ set3/19/main.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+template <char ...chars>
+struct Chars
+{};                    
+
+template <char ch>
+struct OneChar
+{};
+
+template< typename lhs, typename rhs >
+struct Merge;                                   //'basis' template of Merge
+
+template <char ... lhs, char ... rhs >          //specialization Chars & Chars
+struct Merge< Chars< lhs ... >, Chars< rhs ... > >
+{ 
+    typedef Chars< lhs ..., rhs ... > CP; 
+};
+
+template <char ... lhs, char rhs >             //specialization Chars & OneChar
+struct Merge< Chars< lhs ... >, OneChar<rhs> >
+{ 
+    typedef Chars< lhs ..., rhs> CP; 
+};
+
+template<char ...chars>
+ostream& operator<<(ostream& out, Chars<chars...> insert)
+{
+    return (out << ... << chars);               //Simple insert in ostream for this exercise!
+}
+
+int main()
+{
+
+    cout << 
+        Merge<
+            Chars<'1', '2', '3'>, 
+            Merge<Chars<'4', '5'>, OneChar<'6'>>::CP
+        >::CP{} << '\n';
+    
+}
+        
