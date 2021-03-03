586bd7b5a38b4a3d036c648c21895b41127c349e
Timen van Gelderen <tvgelderen@outlook.com>
2021-03-03 14:50:35 +0100

    fixed ICI, added const ptr function

--- set4/28/main.cc
+++ set4/28/main.cc
@@ -5,7 +5,13 @@
 using namespace std;
 
 template<typename Type>
-Type * const makeConst(Type val)
+void checkType(Type p)
+{
+    cout << Traits<Type>::value << '\n';
+}
+
+template<typename Type>
+Type * const makeConstPtr(Type val)
 {
     return new Type{ val };
 }
@@ -16,6 +22,16 @@ int main()
          << Traits<int*>::value << '\n'
          << Traits<int&>::value << '\n'
          << Traits<int&&>::value << '\n';
+
+    int basic = 5;
+    int &ref = basic;
+    int *pointer = &basic;
+    int &&rref = std::move(basic);
+
+    checkType(basic);
+    checkType(ref);
+    checkType(pointer);
+    checkType(std::move(rref));
 
     auto ptr = makeConstPtr(42);
     cout << "ptr value: " << *ptr << '\n';
