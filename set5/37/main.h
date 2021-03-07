74825bc6d56229e6d6e0ebd825aca41ca442f07d
Timen van Gelderen <tvgelderen@outlook.com>
2021-03-07 15:46:38 +0100

    initial 37

---
+++ set5/37/main.h
@@ -0,0 +1,24 @@
+#include <concepts>
+
+template <typename Type>
+concept Add =
+    requires(Type type)
+    {
+        type + type;
+        type - type;
+    };
+
+template <typename Type>
+concept Mul =
+    requires(Type type)
+    {
+        type * type;
+        type / type;
+    };
+
+template <typename Type>
+concept AddMul = Add<Type> or Mul<Type>;
+
+template <typename Type>
+concept AddOrMul = (Add<Type> and not Mul<Type>)
+    or (not Add<Type> and Mul<Type>);
