f29926fe3ad39a1161607b088525d1ef6db99ef3
Timen van Gelderen <tvgelderen@outlook.com>
2021-03-08 08:44:05 +0100

    updated 25

--- set4/25/insertable.h
+++ set4/25/insertable.h
@@ -1,17 +1,20 @@
 #ifndef INCLUDED_INSERTABLE_
 #define INCLUDED_INSERTABLE_
 
-template <template <typename> class Container, typename Type>
-struct Insertable: public Container<Type>
+template <template <typename ...> class Container, typename ...Types>
+struct Insertable: public Container<Types ...>
 {
     Insertable();
     Insertable(Insertable const &other);
     Insertable(Insertable &&tmp);
 
-    Insertable(Container<Type> const &data);
-    Insertable(Container<Type> &&data);
-    Insertable(Type const &value);
-    Insertable(Type &&value);
+    template <typename ...Params>
+    Insertable(Params ...params);
+
+    Insertable(Container<Types ...> const &data);
+    Insertable(Container<Types ...> &&data);
+    Insertable(Types const &...values);
+    Insertable(Types &&...values);
 };
 
 #endif
