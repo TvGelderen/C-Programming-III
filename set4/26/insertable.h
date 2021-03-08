bff3a93a380bc96edc45753082d4488b754071d2
Timen van Gelderen <tvgelderen@outlook.com>
2021-03-08 08:44:17 +0100

    updated 26

--- set4/26/insertable.h
+++ set4/26/insertable.h
@@ -3,70 +3,81 @@
 
 #include <ostream>
 
-template <template <typename> class Container, typename Type>
-struct Insertable: public Container<Type>
+template <template <typename ...> class Container, typename ...Types>
+struct Insertable: public Container<Types ...>
 {
     Insertable();
     Insertable(Insertable const &other);
     Insertable(Insertable &&tmp);
 
-    Insertable(Container<Type> const &data);
-    Insertable(Container<Type> &&data);
-    Insertable(Type const &value);
-    Insertable(Type &&value);
+    template <typename ...Params>
+    Insertable(Params ...params);
+
+    Insertable(Container<Types ...> const &data);
+    Insertable(Container<Types ...> &&data);
+    Insertable(Types const &...values);
+    Insertable(Types &&...values);
 };
                                                 // default constructor
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable()
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable()
 :
-    Container<Type>()
+    Container<Types ...>()
 {}
                                                 // copy constructor
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable(
-                                Insertable<Container, Type> const &other)
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable(
+                                Insertable<Container, Types ...> const &other)
 :
-    Container<Type>(other)
+    Container<Types ...>(other)
 {}
                                                 // move constructor
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable(
-                                Insertable<Container, Type> &&tmp)
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable(
+                                Insertable<Container, Types ...> &&tmp)
 :
-    Insertable(tmp)
+    Insertable(std::move(tmp))
 {}
 
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable(Container<Type> const &data)
+                                        // all other possible constructors
+template <template <typename ...> class Container, typename ...Types>
+template <typename ...Params>
+inline Insertable<Container, Types ...>::Insertable(Params ...params)
 :
-    Container<Type>(data)
+    Insertable(std::forward<Params>(params)...)
 {}
 
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable(Container<Type> &&data)
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable(Container<Types ...> const &data)
+:
+    Container<Types ...>(data)
+{}
+
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable(Container<Types ...> &&data)
 :
     Insertable(data)
 {}
 
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable(Type const &value)
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable(Types const &...values)
 :
-    Container<Type>(1, value)
+    Insertable(std::forward<Types>(values)...)
 {}
 
-template <template <typename> class Container, typename Type>
-inline Insertable<Container, Type>::Insertable(Type &&value)
+template <template <typename ...> class Container, typename ...Types>
+inline Insertable<Container, Types ...>::Insertable(Types &&...values)
 :
-    Insertable(value)
+    Container<Types ...>(1, std::forward<Types>(values)...)
 {}
 
                                     // Insertable insertion operator
-template <template <typename> class Container, typename Type>
+template <template <typename ...> class Container, typename ...Types>
 inline std::ostream &operator<<(std::ostream &out,
-                                Insertable<Container, Type> const &obj)
+                                Insertable<Container, Types ...> const &obj)
 {
-    for (size_t idx = 0; idx != obj.size(); ++idx)
-        out << obj[idx] << ' ';
+    for (auto iter = obj.begin(); iter != obj.end(); ++iter)
+        out << *iter << ' ';
 
     return out;
 }
