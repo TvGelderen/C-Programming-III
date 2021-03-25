af395850c81c0e5e6982e1e8ccbf87a7c7cb4e9d
swenjohannes <s.johannes@student.rug.nl>
2021-03-25 21:03:44 +0100

    seems legit

--- set7/50/main.cc
+++ set7/50/main.cc
@@ -1,21 +1,7 @@
 #include "parser/parser.h"
-#include <iostream>
-
-using namespace std;
 
 int main()
 {
-    /*
-    Scanner scanner;
-    while (int token = scanner.lex())
-    {
-        cout << token << ':' << scanner.matched();           
-    }
-    cout << '\n';               //close the line!
-    */
-
-
     Parser parser;
-    //parser.setDebug(true);
     return parser.parse();
 }
